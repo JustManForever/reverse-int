f40221c10491c334a7bce90c1d009b3e97a78f7a
JustManForever <70839428+JustManForever@users.noreply.github.com>
2020-10-18 16:02:32 +0400

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let a =  Array.from("" + Math.abs(int)).reverse();
+    console.log(a)
+    if(a.indexOf('0') === 0) {
+        a.shift()
+    }
+
+    return Number.parseInt(a.join(''))
 }
