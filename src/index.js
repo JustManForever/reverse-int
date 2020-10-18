17f28e20206fd685c4c94278f29cacfd49efa940
JustManForever <70839428+JustManForever@users.noreply.github.com>
2020-10-18 16:15:43 +0400

    fix: replacing the name of a function variable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function reverse (n) {
+module.exports = function reverse (int) {
   let a =  Array.from("" + Math.abs(int)).reverse();
     console.log(a)
     if(a.indexOf('0') === 0) {
